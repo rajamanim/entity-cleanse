555ea2d1a02588d4f752e2ad32156c5f03b34900
mahesh.rajamani <mahesh.rajamani@mezocliq.com>
2015-02-09 13:29:09 -0500

    Changes to upper

--- src/main/java/com/entitycleanse/interfaces/CleanseFactory.java
+++ src/main/java/com/entitycleanse/interfaces/CleanseFactory.java
@@ -2,6 +2,6 @@ package com.entitycleanse.interfaces;
 
 public class CleanseFactory {
 	public static String cleanAddressLine1(String addressLine) {
-		return addressLine.toLowerCase();
+		return addressLine.toUpperCase();
 	}
 }
